161b02de0fc245f223ddba0fc26f135a59b19d96
Jonn <jonnutri@gmail.com>
2023-08-21 20:33:44 -0300

    feat: Nova Categoria - req06

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from news.views import index, details
+from news.views import index, details, category_form
 
 
 urlpatterns = [
     path("", index, name="home-page"),
     path("news/<int:id>", details, name="news-details-page"),
+    path("categories/", category_form, name="categories-form"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from news.models import News
+from news.forms import CategoryForm
 
 
 def index(request):
@@ -12,3 +13,16 @@ def details(request, id):
     news = News.objects.get(id=id)
     context = {"news": news}
     return render(request, "news_details.html", context)
+
+
+def category_form(request):
+    form = CategoryForm()
+
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home-page")
+
+    context = {"form": form}
+    return render(request, "categories_form.html", context)
